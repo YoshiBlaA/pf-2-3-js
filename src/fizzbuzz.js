058473f81c5f88abf27db24b1bdb15569161bb08
YoshiBlaA <corts.bombo@gmail.com>
2026-08-28 13:25:06 -0600

    Solve laboratory exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,16 @@
-export function fizzbuzz(){
-  //tu código va aqui
- 
-  
+export function fizzbuzz() {
+  for (let number = 1; number <= 100; number++) {
+    let replaceWord = "";
+
+    if (number % 3 === 0) {
+      replaceWord += 'Fizz';
+    }
+    if (number % 5 === 0) {
+      replaceWord += 'Buzz';
+    }
+    
+    console.log(replaceWord ? replaceWord : number);
+  }
 }
 
 fizzbuzz();
