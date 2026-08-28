7fbcecea09e2244495955a10a46098cae245a9eb
YoshiBlaA <corts.bombo@gmail.com>
2026-08-28 13:30:29 -0600

    Format code

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,14 +1,12 @@
 export function fizzbuzz() {
   for (let number = 1; number <= 100; number++) {
     let replaceWord = "";
-
     if (number % 3 === 0) {
       replaceWord += 'Fizz';
     }
     if (number % 5 === 0) {
       replaceWord += 'Buzz';
     }
-    
     console.log(replaceWord ? replaceWord : number);
   }
 }
